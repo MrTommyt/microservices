2f491b5db0d62a18571620abae9cb5d61028a9de
Carlos <cacastrillon@unimagdalena.edu.co>
2025-03-05 20:27:13 -0500

    New Interface Service

--- product/src/main/java/co/edu/unimagdalena/cbenavides/product/service/ProductService.java
+++ product/src/main/java/co/edu/unimagdalena/cbenavides/product/service/ProductService.java
@@ -1,4 +1,18 @@
 package co.edu.unimagdalena.cbenavides.producto.service;
 
-public class ProductService {
+
+import co.edu.unimagdalena.cbenavides.producto.dto.ProductDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    List<ProductDto> findAll();
+    Mono<ProductDto> findById(UUID id);
+    ProductDto create(ProductDto productDto);
+    Flux<ProductDto> update(int id, ProductDto newProductDto);
+    void delete(int id);
+    List<ProductDto> findByName(String name);
 }
