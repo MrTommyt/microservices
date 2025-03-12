50ff778776ff05959842e70fbe4ab8294c9faff5
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-03-11 19:15:46 -0500

    change name method of "create" to "save"

--- product/src/main/java/co/edu/unimagdalena/cbenavides/product/service/ProductService.java
+++ product/src/main/java/co/edu/unimagdalena/cbenavides/product/service/ProductService.java
@@ -6,14 +6,13 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
 public interface ProductService {
     Flux<ProductDto> findAll();
     Mono<ProductDto> findById(UUID id);
-    Mono<ProductDto> create(ProductDto productDto);
+    Mono<ProductDto> save(ProductDto productDto);
     Mono<ProductDto> update(UUID id, ProductDto newProductDto);
     void delete(UUID id);
     Mono<ProductDto> findByName(String name);
