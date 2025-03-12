a760255283b64da1d455d5d56195dbb4535a0256
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-03-12 01:10:18 -0500

    methods getters and setters createds

--- product/src/main/java/co/edu/unimagdalena/cbenavides/product/entity/Product.java
+++ product/src/main/java/co/edu/unimagdalena/cbenavides/product/entity/Product.java
@@ -16,6 +16,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private UUID id;
 
     @Column(name = "name")
@@ -30,4 +31,43 @@ public class Product {
     @Column(name = "description")
     private String description;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
