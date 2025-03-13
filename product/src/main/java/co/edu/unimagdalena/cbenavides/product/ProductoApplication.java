e34366e118949a90c518f10adbe9744115b5e92e
Camilo Benavides <60544121+MrTommyt@users.noreply.github.com>
2025-03-13 08:37:24 -0500

    Setup discovery client on services

--- product/src/main/java/co/edu/unimagdalena/cbenavides/product/ProductoApplication.java
+++ product/src/main/java/co/edu/unimagdalena/cbenavides/product/ProductoApplication.java
@@ -2,8 +2,10 @@ package co.edu.unimagdalena.cbenavides.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductoApplication {
 
     public static void main(String[] args) {
