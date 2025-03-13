efedc02aba9823de0be94fe434df629861b189a3
Camilo Benavides <60544121+MrTommyt@users.noreply.github.com>
2025-03-12 20:43:05 -0500

    Setup discovery server

---
+++ discovery/src/test/java/co/edu/unimagdalena/cbenavides/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unimagdalena.cbenavides.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
