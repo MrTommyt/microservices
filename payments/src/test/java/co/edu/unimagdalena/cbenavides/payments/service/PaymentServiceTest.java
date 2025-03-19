343f22125f92bd5120fddede99a74d3fff8937a8
mcamilamp <camimerpa@gmail.com>
2025-03-19 14:55:48 -0500

    all tests from PaymentServiceTest

--- payments/src/test/java/co/edu/unimagdalena/cbenavides/payments/service/PaymentServiceTest.java
+++ payments/src/test/java/co/edu/unimagdalena/cbenavides/payments/service/PaymentServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -62,19 +63,50 @@ class PaymentServiceTest {
 
     @Test
     void findById() {
-        // aaa
-        // aaa
+        UUID id = payment.getId();
+
+        // cuando se llame .findById entonces retorna un mono que contiene el objeto payment
+        Mockito.when(paymentService.findById(id)).thenReturn(Mono.just(payment));
+
+        Mono<PaymentDto> result = paymentService.findById(id);
+        // como Mono es reactiva entonces se usa .block para obtener su valor de forma sincronica
+        PaymentDto paymentDto = result.block();
+
+        assertNotNull(paymentDto);
+        assertEquals(paymentDto.getId(), payment.getId());
     }
 
     @Test
     void save() {
+        Mockito.when(paymentService.save(payment)).thenReturn(Mono.just(payment));
+
+        Mono<PaymentDto> result = paymentService.save(payment);
+        PaymentDto paymentDto = result.block();
+
+        assertNotNull(paymentDto);
+        assertEquals(paymentDto.getId(), payment.getId());
     }
 
     @Test
     void update() {
+        UUID id = payment.getId();
+        PaymentDto updatedPayment = new PaymentDto(id, 10.0, LocalDateTime.now(), "Credit", "mm");
+
+        Mockito.when(paymentService.update(id, updatedPayment)).thenReturn(Mono.just(updatedPayment));
+
+        Mono<PaymentDto> result = paymentService.update(id, updatedPayment);
+        PaymentDto paymentDto = result.block();
+
+        assertNotNull(paymentDto);
+        assertEquals(10.0, paymentDto.getAmount());
+        assertEquals(paymentDto.getId(), payment.getId());
     }
 
     @Test
     void delete() {
+        UUID id = payment.getId();
+        Mockito.doNothing().when(paymentService).delete(id);
+
+        assertDoesNotThrow( () -> paymentService.delete(id));
     }
 }
