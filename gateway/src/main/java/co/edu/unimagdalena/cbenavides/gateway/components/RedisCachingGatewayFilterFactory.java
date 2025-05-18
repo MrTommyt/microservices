2a0a33f91a4cbad29a8f6e516afa13731a6a9706
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-05-18 08:23:43 -0500

    correction package name

--- gateway/src/main/java/co/edu/unimagdalena/cbenavides/gateway/components/RedisCachingGatewayFilterFactory.java
+++ gateway/src/main/java/co/edu/unimagdalena/cbenavides/gateway/components/RedisCachingGatewayFilterFactory.java
@@ -1,4 +1,4 @@
-package com.example.gateway;
+package co.edu.unimagdalena.cbenavides.gateway.components;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
