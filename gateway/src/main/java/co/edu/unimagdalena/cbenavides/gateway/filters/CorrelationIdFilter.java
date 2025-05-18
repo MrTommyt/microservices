1d13e6a285955dc9ca229ec6e6ee161fd52c4947
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2025-05-18 08:19:50 -0500

    CorrelationIdFilter fixed

--- gateway/src/main/java/co/edu/unimagdalena/cbenavides/gateway/filters/CorrelationIdFilter.java
+++ gateway/src/main/java/co/edu/unimagdalena/cbenavides/gateway/filters/CorrelationIdFilter.java
@@ -2,6 +2,7 @@ package co.edu.unimagdalena.cbenavides.gateway.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,11 +11,36 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Component
-public class CorrelationIdFilter implements GatewayFilter {
+public class CorrelationIdFilter implements GatewayFilter, Ordered {
+    private static final String CORRELATION_ID = "X-Correlation-ID";
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest req = exchange.getRequest();
-        req.getHeaders().add("X-Correlation-ID", UUID.randomUUID().toString());
-        return chain.filter(exchange);
+        // Verificar si ya viene un correlationId en los headers
+        String correlationId = exchange.getRequest().getHeaders().getFirst(CORRELATION_ID);
+
+        // Si no viene un correlationId entonces se genera uno nuevo
+        if (correlationId == null || correlationId.isEmpty()) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                .mutate()
+                .header(CORRELATION_ID, correlationId)
+                .build();
+
+        return chain.filter(exchange.mutate().request(mutatedRequest).build());
     }
+
+    private String getCorrelationId(ServerWebExchange exchange) {
+        return exchange.getRequest().getHeaders().getFirst(CORRELATION_ID);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
 }
+
+
